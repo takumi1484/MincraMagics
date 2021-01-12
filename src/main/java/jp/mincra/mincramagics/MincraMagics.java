88e3990cedf9d380a58d7d809ed9bd8654f7d164
Harineko0 <harineko0927@gmail.com>
2021-01-13 03:48:31 +0900

    コミットできない(´・ω・｀)

--- src/main/java/jp/mincra/mincramagics/MincraMagics.java
+++ src/main/java/jp/mincra/mincramagics/MincraMagics.java
@@ -4,38 +4,41 @@ import jp.mincra.mincramagics.util.PropertyUtil;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
-import java.util.ResourceBundle;
 
 public final class MincraMagics extends JavaPlugin {
 
     private static PlayerManager pManager;
-
-    public static Map<String, String> PropertyMap;
+    private static Map<String, String> PropertyMap;
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
         //プロパティ
         try {
             PropertyUtil.setProperty();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        PropertyUtil.loadProperty();
+        PropertyUtil.getProperty();
 
         getPlayerManager();
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
     }
 
-    //PlayerManagerをインスタンス化
+    //pManagerを取得
     public static PlayerManager getPlayerManager() {
         if (pManager == null)
             pManager = new PlayerManager();
         return pManager;
+    }
+    //PropertyMapを取得
+    public static Map<String, String> getPropertyMap() {
+        if (PropertyMap == null)
+            PropertyMap = new HashMap<>();
+        return PropertyMap;
     }
 }
