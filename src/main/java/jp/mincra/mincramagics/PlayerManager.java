c9de4258b93e642202fa0aeb144605e7d411566f
Harineko0 <harineko0927@gmail.com>
2021-01-13 03:18:54 +0900

    PlayerManager.javaを追加。MincraPlayerUUIDMapから各種変数を取得するメソッドを追加。

---
+++ src/main/java/jp/mincra/mincramagics/PlayerManager.java
@@ -0,0 +1,40 @@
+package jp.mincra.mincramagics;
+
+import jp.mincra.mincramagics.container.MincraPlayer;
+
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class PlayerManager {
+
+    private ConcurrentHashMap<UUID, MincraPlayer> MincraPlayerUUIDMap = new ConcurrentHashMap<>();
+
+    public void putMincraPlayer(MincraPlayer mincraPlayer) {
+        MincraPlayerUUIDMap.put(mincraPlayer.getPlayerUUID(),mincraPlayer);
+    }
+    public void removeMincraPlayer(UUID uuid) {
+        MincraPlayerUUIDMap.remove(uuid);
+    }
+
+    //MP
+    public int getPlayerMP(UUID uuid) {
+        return MincraPlayerUUIDMap.get(uuid).getPlayerMP();
+    }
+    public void addPlayerMP(UUID uuid, int mp) {
+        MincraPlayer mincraPlayer = MincraPlayerUUIDMap.get(uuid);
+        mp = mp + mincraPlayer.getPlayerMP();
+        mincraPlayer.setPlayerMP(mp);
+        MincraPlayerUUIDMap.put(uuid,mincraPlayer);
+    }
+
+    //Cooltime
+    public int getPlayerCooltime(UUID uuid) {
+        return MincraPlayerUUIDMap.get(uuid).getPlayerCooltime();
+    }
+    public void addPlayerCooltime(UUID uuid, int cooltime) {
+        MincraPlayer mincraPlayer = MincraPlayerUUIDMap.get(uuid);
+        cooltime = cooltime + mincraPlayer.getPlayerCooltime();
+        mincraPlayer.setPlayerMP(cooltime);
+        MincraPlayerUUIDMap.put(uuid,mincraPlayer);
+    }
+}
