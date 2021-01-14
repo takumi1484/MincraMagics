1f4e3f86c264dd22c1066463ef9c576f3b8d5803
Harineko0 <harineko0927@gmail.com>
2021-01-14 20:09:13 +0900

    [MODIFY] ログイン時にSQLからデータを読み込むように変更。

--- src/main/java/jp/mincra/mincramagics/listeners/MincraListener.java
+++ src/main/java/jp/mincra/mincramagics/listeners/MincraListener.java
@@ -2,26 +2,41 @@ package jp.mincra.mincramagics.listeners;
 
 import jp.mincra.mincramagics.MincraMagics;
 import jp.mincra.mincramagics.container.MincraPlayer;
-import org.bukkit.GameMode;
-import org.bukkit.Material;
-import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
-import org.bukkit.event.player.PlayerToggleFlightEvent;
-import org.bukkit.util.Vector;
 
 import java.util.UUID;
 
 public class MincraListener implements Listener {
 
     @EventHandler
-    public void onPlayerLogin(PlayerLoginEvent event) {
-        System.out.println("login");
-        UUID uuid = event.getPlayer().getUniqueId();
-        MincraPlayer mincraPlayer = MincraMagics.getSQLManager().getMincraPlayer(uuid);
-        System.out.println("[MincraMagics] name:"+mincraPlayer.getPlayerName()+" mp:"+mincraPlayer.getPlayerMP());
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+
+        if (event.getPlayer().hasPlayedBefore()){
+            //1度ログイン済み
+
+            //SQLから情報を取得
+            MincraPlayer mincraPlayer = MincraMagics.getSQLManager().getMincraPlayer(event.getPlayer().getUniqueId());
+            //プレイヤーをHashMapに追加
+            MincraMagics.getPlayerManager().putMincraPlayer(mincraPlayer);
+
+        } else {
+            //初回ログイン
+            System.out.println("[MincraMagics] "+player.getName()+"が初めてログインしました！");
+
+            //新規のインスタンス
+            MincraPlayer mincraPlayer = new MincraPlayer();
+            mincraPlayer.setPlayerUUID(player.getUniqueId());
+            mincraPlayer.setPlayerName(player.getName());
+            //SQLに追加 (いらない処理)
+            MincraMagics.getSQLManager().insertMincraPlayer(player.getUniqueId(),mincraPlayer);
+            //プレイヤーをHashMapに追加
+            MincraMagics.getPlayerManager().putMincraPlayer(mincraPlayer);
+        };
     }
 
     /**
