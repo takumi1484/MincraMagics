b1c7590642e4456aab8a0c9de242c836f9674162
Harineko0 <harineko0927@gmail.com>
2021-01-20 00:07:46 +0900

    [ADD] MincraChatUtilを追加。Hexカラーコードの対応・デバッグメッセージなど。

---
+++ src/main/java/jp/mincra/mincramagics/util/MincraChatUtil.java
@@ -0,0 +1,45 @@
+package jp.mincra.mincramagics.util;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MincraChatUtil {
+
+    private final static Pattern HEX_PATTERN = Pattern.compile("&(#[A-Fa-f0-9]{6})");
+    private final static char COLOR_CHAR = ChatColor.COLOR_CHAR;
+
+    public static String translateHexColorCodes(String message) {
+        //Sourced from this post by imDaniX: https://github.com/SpigotMC/BungeeCord/pull/2883#issuecomment-653955600
+        Matcher matcher = HEX_PATTERN.matcher(message);
+        StringBuffer buffer = new StringBuffer(message.length() + 4 * 8);
+        while (matcher.find()) {
+            String group = matcher.group(1);
+            matcher.appendReplacement(buffer, COLOR_CHAR + "x"
+                    + COLOR_CHAR + group.charAt(0) + COLOR_CHAR + group.charAt(1)
+                    + COLOR_CHAR + group.charAt(2) + COLOR_CHAR + group.charAt(3)
+                    + COLOR_CHAR + group.charAt(4) + COLOR_CHAR + group.charAt(5)
+            );
+        }
+        return ChatColor.translateAlternateColorCodes('&',matcher.appendTail(buffer).toString());
+    }
+
+    /**
+     * カラーコードを翻訳する。&修飾子はそのまま使える。
+     * @param msg usage: &#FF0000&fテキスト
+     */
+    public static String makeDebugMessage(String msg) {
+        String prefix = "&#3ebef5&f[&#3de8fe&fMincraMagics&#3ebef5&f] &r";
+        StringBuilder buf = new StringBuilder();
+        buf.append(prefix);
+        buf.append(msg);
+
+        return translateHexColorCodes(buf.toString());
+    }
+
+    public static void sendConsoleMessage(String msg) {
+        Bukkit.getServer().getConsoleSender().sendMessage(translateHexColorCodes(makeDebugMessage(msg)));
+    }
+}
