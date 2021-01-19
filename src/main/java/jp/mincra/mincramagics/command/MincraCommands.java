8165e7422c787374202af22aa73f4aa0a90c5353
Harineko0 <harineko0927@gmail.com>
2021-01-20 02:55:18 +0900

    [ADD] テストとして/mcr時にメインハンドのアイテムのNBTを改変するように。

--- src/main/java/jp/mincra/mincramagics/command/MincraCommands.java
+++ src/main/java/jp/mincra/mincramagics/command/MincraCommands.java
@@ -1,13 +1,16 @@
 package jp.mincra.mincramagics.command;
 
+import de.tr7zw.changeme.nbtapi.NBTCompound;
+import de.tr7zw.changeme.nbtapi.NBTItem;
 import jp.mincra.mincramagics.MincraMagics;
 import jp.mincra.mincramagics.entity.player.PlayerManager;
 import jp.mincra.mincramagics.util.MincraChatUtil;
-import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class MincraCommands implements CommandExecutor {
@@ -22,10 +25,23 @@ public class MincraCommands implements CommandExecutor {
         Player caster;
         if (sender instanceof Player) {
             caster = (Player) sender;
-            caster.sendMessage(MincraChatUtil.makeDebugMessage(args[0]));
             PlayerManager playerManager = MincraMagics.getPlayerManager();
-            playerManager.addPlayerMP_value(caster.getUniqueId(), 20);
+            playerManager.addPlayerMP_value(caster.getUniqueId(), -20);
             playerManager.setPlayerCooltime(caster.getUniqueId(), 10);
+
+            ItemStack itemStack = caster.getInventory().getItemInMainHand();
+            NBTItem nbtItem = new NBTItem(itemStack);
+            caster.sendMessage(MincraChatUtil.makeDebugMessage(nbtItem.toString()));
+            NBTCompound nbtCompound = nbtItem.addCompound("MincraMagics");
+            nbtCompound.setString("id","daiyanoken");
+            caster.sendMessage(MincraChatUtil.makeDebugMessage(nbtCompound.toString()));
+            caster.sendMessage(MincraChatUtil.makeDebugMessage(nbtItem.toString()));
+
+            itemStack = nbtItem.getItem();
+            int slot = caster.getInventory().getHeldItemSlot();
+            caster.getInventory().setItem(slot,itemStack);
+
+            return true;
         }
         return false;
     }
