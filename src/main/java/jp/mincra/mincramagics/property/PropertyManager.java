a888de4afb2fb708058fc26a9b5df60ed58cc22a
Harineko0 <harineko0927@gmail.com>
2021-01-20 19:39:49 +0900

    [MODIFY] プラグイン起動時にdata/items.jsonが生成されるように。

--- src/main/java/jp/mincra/mincramagics/property/PropertyManager.java
+++ src/main/java/jp/mincra/mincramagics/property/PropertyManager.java
@@ -30,23 +30,69 @@ public class PropertyManager {
     }
 
     public void setProperty() {
-        File dir = new File("./plugins/MincraMagics");
+        //Property
+        File mainDir = new File("./plugins/MincraMagics");
         File properties = new File("./plugins/MincraMagics/mincra.properties");
 
         //ディレクトリ作成
-        if (dir.exists() == false) {
-            dir.mkdir();
+        if (mainDir.exists() == false) {
+            mainDir.mkdir();
         }
 
         if (properties.exists() == false){
-            FileWriter fileWriter = null;
             try {
+                FileWriter fileWriter;
                 fileWriter = new FileWriter(properties);
                 fileWriter.write("MySQL_url=jdbc:mysql://localhost/mincra\n" +
                         "MySQL_user=root\n" +
                         "MySQL_password=suken314");
                 fileWriter.close();
                 properties.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        //Json
+        File jsonItemsDir = new File("./plugins/MincraMagics/data");
+        File jsonItemFile = new File("./plugins/MincraMagics/data/items.json");
+
+        if (jsonItemsDir.exists() == false) {
+            jsonItemsDir.mkdir();
+        }
+
+        if (jsonItemFile.exists() == false) {
+            try {
+                FileWriter fileWriter;
+                fileWriter = new FileWriter(jsonItemFile);
+                fileWriter.write("[\n" +
+                        "  {\n" +
+                        "    \"id\": \"apple\",\n" +
+                        "    \"mcr_id\": \"example\",\n" +
+                        "    \"nbt\": {\n" +
+                        "      \"display\":{\n" +
+                        "        \"Name\": \"{\\\"text\\\":\\\"Example Item\\\",\\\"color\\\":\\\"white\\\",\\\"bold\\\":true,\\\"italic\\\":false}\",\n" +
+                        "        \"Lore\":[\n" +
+                        "          \"{\\\"text\\\":\\\"This is a example item.\\\",\\\"color\\\":\\\"gray\\\",\\\"italic\\\":false}\"]},\n" +
+                        "      \"CustomModelData\":114514,\n" +
+                        "      \"MincraMagics\":{\n" +
+                        "        \"id\":\"example\"}\n" +
+                        "    },\n" +
+                        "    \"recipe\": {\n" +
+                        "      \"shape\": [\n" +
+                        "        \" a \",\n" +
+                        "        \"aba\",\n" +
+                        "        \" a \"],\n" +
+                        "      \"ingredient\": {\n" +
+                        "        \"a\": \"red_dye\",\n" +
+                        "        \"b\": \"apple\"\n" +
+                        "      }\n" +
+                        "    }\n" +
+                        "  }\n" +
+                        "]");
+                fileWriter.close();
+                jsonItemFile.createNewFile();
+
             } catch (IOException e) {
                 e.printStackTrace();
             }
