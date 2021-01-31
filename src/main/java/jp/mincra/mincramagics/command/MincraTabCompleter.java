c0a54c28bee11743d884adb9391a5100995cf8a9
Harineko0 <harineko0927@gmail.com>
2021-01-31 13:58:21 +0900

    [MODIFY] TabCompleterの予測変換を修正。

--- src/main/java/jp/mincra/mincramagics/command/MincraTabCompleter.java
+++ src/main/java/jp/mincra/mincramagics/command/MincraTabCompleter.java
@@ -1,31 +1,41 @@
 package jp.mincra.mincramagics.command;
 
 import jp.mincra.mincramagics.MincraMagics;
-import jp.mincra.mincramagics.util.ChatUtil;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
-import org.bukkit.util.StringUtil;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class MincraTabCompleter implements TabCompleter {
+
+
 
     @Override
     public List<String> onTabComplete(CommandSender sender, Command cmd, String label, String[] args){
 
         if (args.length == 1) {
-            return Arrays.asList("give","reload");
+            final List<String> arg1List = new ArrayList<>(Arrays.asList("give","reload","cooltime"));
 
+            List<String> completions = new ArrayList<>();
+
+            for (String arg1 : arg1List) {
+                if (arg1.contains(args[0])){
+                    completions.add(arg1);
+                }
+            }
+            return completions;
         }
 
         if (args.length > 1) {
             switch (args[0]) {
                 case "give":
                     return give(args);
+
+                case "cooltime":
+                    return cooltime(args);
 
             }
         }
@@ -39,7 +49,9 @@ public class MincraTabCompleter implements TabCompleter {
         //give Harineko0
         if (args.length == 3) {
             for (String key : MincraMagics.getItemManager().getItemStackMap().keySet()) {
-                completions.add(key);
+                if (key.contains(args[2])) {
+                    completions.add(key);
+                }
             }
             return completions;
         }
@@ -47,4 +59,24 @@ public class MincraTabCompleter implements TabCompleter {
         return null;
     }
 
+    private List<String> cooltime(String[] args) {
+        final List<String> arg2List = new ArrayList<>(Arrays.asList("set"));
+
+        List<String> completions = new ArrayList<>();
+
+        if (args.length == 2) {
+            for (String arg : arg2List) {
+                if (arg.contains(args[1])){
+                    completions.add(arg);
+                }
+            }
+            return completions;
+        } else if (args.length > 3)
+            switch (args[1]) {
+            case "set":
+                return Arrays.asList("");
+        }
+
+        return null;
+    }
 }
