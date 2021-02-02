b42d59d343fc931818a43ef750f499353b21e8f0
Harineko0 <harineko0927@gmail.com>
2021-02-02 23:47:13 +0900

    [ADD] DestroyRodの型のみ追加。

---
+++ src/main/java/jp/mincra/mincramagics/skill/rod/DestroyRod.java
@@ -0,0 +1,12 @@
+package jp.mincra.mincramagics.skill.rod;
+
+import jp.mincra.mincramagics.event.player.PlayerUseMagicRodEvent;
+import org.bukkit.entity.Player;
+
+public class DestroyRod implements PlayerUseMagicRodEvent {
+
+    @Override
+    public void onPlayerUseMagicRod(Player player, String mcr_id) {
+
+    }
+}
