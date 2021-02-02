e3a9dca3bd316ef06ceb8ba3f3510da4d2c88fd8
Harineko0 <harineko0927@gmail.com>
2021-02-02 23:19:23 +0900

    [MODIFY] ExampleZombieMobにアニメーションパーティクルを追加。

--- src/main/java/jp/mincra/mincramagics/entity/mob/hostile/ExampleZombieMob.java
+++ src/main/java/jp/mincra/mincramagics/entity/mob/hostile/ExampleZombieMob.java
@@ -1,23 +1,46 @@
 package jp.mincra.mincramagics.entity.mob.hostile;
 
-import de.tr7zw.changeme.nbtapi.NBTContainer;
-import de.tr7zw.changeme.nbtapi.NBTEntity;
-import jp.mincra.mincramagics.MincraMagics;
 import jp.mincra.mincramagics.event.entity.CustomEntitySpawnEvent;
+import jp.mincra.mincramagics.util.MincraParticle;
+import org.bukkit.Particle;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Zombie;
 import org.bukkit.event.entity.EntitySpawnEvent;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class ExampleZombieMob implements CustomEntitySpawnEvent {
 
     @Override
     public void onCustomEntitySpawn(EntitySpawnEvent event, String mcr_id) {
-//
-//        if (mcr_id.equals("example_zombie")) {
-//
-//            Zombie zombie = (Zombie) event.getEntity();
-//
-//            NBTEntity nbtZombie = new NBTEntity(zombie);
-//            nbtZombie.mergeCompound(new NBTContainer(MincraMagics.getMobManager().getMobNBT(mcr_id).toString()));
-//        }
+
+        if (mcr_id.equals("example_zombie")) {
+
+            Zombie zombie = (Zombie) event.getEntity();
+
+            MincraParticle mincraParticle = new MincraParticle();
+            mincraParticle.setParticle(Particle.VILLAGER_HAPPY);
+            mincraParticle.setRadius(4);
+
+            final double[] i = {0};
+
+            Timer timer = new Timer();
+            TimerTask task = new TimerTask() {
+                public void run() {
+                    if (zombie.isValid()) {
+
+                        i[0] += 0.1;
+
+                        mincraParticle.setYaw(i[0]);
+                        mincraParticle.drawMagicCircle(zombie.getLocation(), 5, 1);
+
+                    } else if (zombie.isDead()) {
+                        this.cancel();
+                    }
+                }
+            };
+            timer.scheduleAtFixedRate(task,0,50);
+        }
     }
 }
